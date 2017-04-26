31d1c0ce5284ed387c940fb6c664ecccc9c53e0f
Jesus Vega <chuyspc@Jesuss-MacBook-Air.local>
2017-04-25 22:28:30 -0700

    First Iteration

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
-(function(){
-  console.log("Linked");
-})();
+function overlay() {
+  var el = document.getElementById("overlay");
+
+	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+}
+
+window.onclick = function(event) {
+  el = document.getElementById("overlay");
+  if (event.target == el) {
+      el.style.visibility = "hidden";
+  }
+}
